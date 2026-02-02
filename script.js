bb234ed6e4409e476094f9fc14c0c726238c97af
fotohabilaj <fotohabilaj@gmail.com>
2026-02-02 15:10:05 +0100

    add human code

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("hello");
+
+
+//km? eta? const int promt
+//km*0,21
+//if eta<18(20%) ${numero arrotondato}${euro}
+//else if eta>65(40%) ${numero arrotondato}${euro}
+//else ${numero arrotondato}${euro}
