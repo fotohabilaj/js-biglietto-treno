1a33d504bcf735b7b063487119c8b2d90df904d2
fotohabilaj <fotohabilaj@gmail.com>
2026-02-02 17:10:47 +0100

    sol 1 complete

--- script.js
+++ script.js
@@ -13,22 +13,20 @@ console.log(distance);
 
 
 //km*0,21
-let initialprice = distance*0.21;
+const initialprice = distance*0.21 ;
 
-console.log(initialprice);
+console.log(initialprice) ;
 
-
-
+let price = initialprice
 
 if (age<18) {
-   let price = initialprice-((initialprice/100)*20);
-   console.log(price  + "€");
+    price = initialprice-((initialprice/100)*20) ;
+   
 //if eta<18(20%) ${numero arrotondato}${euro}   
 } else if (age>65) { 
-    let price = initialprice-((initialprice/100)*40);
-   console.log(price + "€");
+    price = initialprice-((initialprice/100)*40) ;
+
 //else if eta>65(40%) ${numero arrotondato}${euro}   
-} else {
-    console.log(initialprice + "€");
-   //else ${numero arrotondato}${euro} 
 }
+
+ console.log(price.toFixed(2) + "€") ;
