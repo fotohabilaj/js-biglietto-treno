ba19dd6865e1a91f03687a9d902617f24b11f7df
fotohabilaj <fotohabilaj@gmail.com>
2026-02-02 15:52:15 +0100

    add €

--- script.js
+++ script.js
@@ -22,13 +22,13 @@ console.log(initialprice);
 
 if (age<18) {
    let price = initialprice-((initialprice/100)*20);
-   console.log(price+"euro");
+   console.log(price  + "€");
 //if eta<18(20%) ${numero arrotondato}${euro}   
 } else if (age>65) { 
     let price = initialprice-((initialprice/100)*40);
-   console.log(price+"euro");
+   console.log(price + "€");
 //else if eta>65(40%) ${numero arrotondato}${euro}   
 } else {
-    console.log(initialprice+"euro");
+    console.log(initialprice + "€");
    //else ${numero arrotondato}${euro} 
 }
