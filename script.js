37b75fc13d2daf544c7a64e2d7cf6ae32ef6c080
fotohabilaj <fotohabilaj@gmail.com>
2026-02-02 15:41:19 +0100

    add under18

--- script.js
+++ script.js
@@ -2,7 +2,27 @@ console.log("hello");
 
 
 //km? eta? const int promt
+
+const age = parseInt(prompt("Quanti anni hai?"));
+const distance = parseInt(prompt("Quanti km devi fare?"));
+
+console.log(age);
+console.log(distance);
+
+
+
+
 //km*0,21
+let initialprice = distance*0.21;
+
+console.log(initialprice);
+
+
+
 //if eta<18(20%) ${numero arrotondato}${euro}
+if(age<18){
+   let price = initialprice-((initialprice/100)*20);
+   console.log(price+"euro");
+}
 //else if eta>65(40%) ${numero arrotondato}${euro}
 //else ${numero arrotondato}${euro}
